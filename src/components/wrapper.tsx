1b1edeb84a25812588654c8514522b1647e590c6
nedoxff <nedoxff@proton.me>
2025-09-03 23:33:27 +0300

    add date-time section & update screenshots for literals

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -6,7 +6,7 @@ export function Wrapper(props: HTMLAttributes<HTMLDivElement>) {
 		<div
 			{...props}
 			className={cn(
-				"justify-center items-center flex rounded-lg bg-radial-[at_bottom] from-blue-500/20 p-0 border border-fd-primary/10 prose-no-margin dark:bg-black/20 [&>*]:max-w-[30%]",
+				"justify-center items-center flex rounded-lg bg-radial-[at_bottom] from-blue-500/20 p-0 border border-fd-primary/10 prose-no-margin dark:bg-black/20 [&_img]:h-[30vh] [&_img]:w-auto",
 				props.className,
 			)}
 		>
